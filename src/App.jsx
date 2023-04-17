20648b74fd88c65d88c557e08e283fb4640ea6f9
kiwy36 <kevinwittner92@gmail.com>
2023-04-17 19:29:17 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Subtitle from './components/subtitle';
+import Navbar from './components/Navbar/Navbar';
+import BotonContador from './components/BotonContador/BotonContador';
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Subtitle/>
+      <BotonContador/>
+    </>
+  )
+}
+
+export default App
